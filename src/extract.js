f2e756bf46e01bf7dfc600e49cfbe6706c009a48
Christoph Klassen <christoph.klassen@intevation.de>
2025-07-26 16:18:42 +0200

    Fix warning
    [@vue/compiler-sfc] compileTemplate now requires the `id` option.

--- src/extract.js
+++ src/extract.js
@@ -64,7 +64,7 @@ function preprocessScript(data, type) {
     }
 
     if (vueFile.template) {
-      const vueTemplate = compileTemplate({source: vueFile.template.content});
+      const vueTemplate = compileTemplate({id: `${Math.random()}`, source: vueFile.template.content});
 
       contents.push({
         content: vueTemplate.code,
